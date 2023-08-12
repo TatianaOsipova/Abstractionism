4c75c54f814cc6d020fd9749fab40cfb1d8ea6c9
Tatiana Osipova <otv120780@gmail.com>
2023-08-12 16:14:31 -0400

    feat: correct menu-burger

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// const menuBtn = document.querySelector('.menu__btn');
+// const menuMobile = document.querySelector('.header__menu-list');
+
+// menuBtn.addEventListener('click', () => {
+//     menuMobile.classList.toggle('menu--open')
+// });
+
+const swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+$(function () {
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+});
